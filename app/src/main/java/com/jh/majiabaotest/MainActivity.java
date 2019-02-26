c23089b223e5daa02125f407aaaf19293765814b
jinhuizxc <1004260403@qq.com>
2019-02-26 12:22:49 +0800

    Initial commit

---
+++ app/src/main/java/com/jh/majiabaotest/MainActivity.java
@@ -0,0 +1,22 @@
+package com.jh.majiabaotest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 参考：
+ * Android马甲包
+ * https://blog.csdn.net/xiao_nian/article/details/79460952
+ *
+ * Build Variant在项目中的应用
+ * https://blog.csdn.net/cai_iac/article/details/52330535?utm_source=blogxgwz5
+ *
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
