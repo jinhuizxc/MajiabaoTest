b31b67dd0d24579d7a409d01f3a1bf61dcfe06ce
j_h <13113017500>
2019-04-19 11:37:25 +0800

    test

--- app/src/main/java/com/jh/majiabaotest/MainActivity.java
+++ app/src/main/java/com/jh/majiabaotest/MainActivity.java
@@ -11,6 +11,11 @@ import android.os.Bundle;
  * Build Variant在项目中的应用
  * https://blog.csdn.net/cai_iac/article/details/52330535?utm_source=blogxgwz5
  *
+ * Android马甲包开发之路
+ * https://www.jianshu.com/p/7af5f17de6c1
+ *
+ * 自己需要深入了解的是如何去改功能点，即同一个项目不同包名的apk拥有各自的功能节点；
+ *
  */
 public class MainActivity extends AppCompatActivity {
 
